860f6ea7c46ad7d4c99a640d2f807586ffab3978
fdimarzo <franciscodimarzo@hotmail.com>
2024-12-02 11:59:15 -0300

    added kitty conf to home manager

--- home/home.nix
+++ home/home.nix
@@ -10,18 +10,15 @@
     ripgrep
     fd
     kitty
-    orca-slicer
     just
+    linux-wifi-hotspot
+    orca-slicer
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
+    ".config/kitty".source = ./kitty;
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
