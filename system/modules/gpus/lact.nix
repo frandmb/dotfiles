2c902bfa798a40e7903c83f7f27876d48014b73e
fdimarzo <franciscodimarzo@hotmail.com>
2025-06-01 12:03:10 -0300

    use lact service

--- system/modules/gpus/lact.nix
+++ system/modules/gpus/lact.nix
@@ -1,7 +1,5 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    lact
-  ];
-  systemd.packages = with pkgs; [lact];
-  systemd.services.lactd.wantedBy = ["multi-user.target"];
+{...}: {
+  services.lact = {
+    enable = true;
+  };
 }
