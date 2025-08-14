c4efd796a3a0b55d9433a8b3269c0facb79f9060
fdimarzo <franciscodimarzo@hotmail.com>
2025-08-14 18:40:29 -0300

    set nu as rio default shell and removed noisetorch

--- home/devtools.nix
+++ home/devtools.nix
@@ -40,7 +40,12 @@ in {
     stylua
   ];
 
-  programs.nushell.enable = true;
+  programs.nushell = {
+    enable = true;
+    extraConfig = ''
+      $env.config.show_banner = false
+    '';
+  };
 
   home.file = dotfilesConfig;
 }
