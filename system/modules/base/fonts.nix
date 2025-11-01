4ea321f5c7f8c508264e6a6e4b459ca357240c6f
fdimarzo <23535289+frandmb@users.noreply.github.com>
2025-11-01 12:41:30 -0300

    flake up

--- system/modules/base/fonts.nix
+++ system/modules/base/fonts.nix
@@ -1,12 +1,8 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
-    noto-fonts-emoji
+    noto-fonts-color-emoji
     liberation_ttf
     nerd-fonts.meslo-lg
   ];
