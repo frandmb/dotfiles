1196d873651446c52c0a89e90cc1392fcabd16bb
fdimarzo <23535289+frandmb@users.noreply.github.com>
2025-11-28 17:33:07 -0300

    enable kdeconnect ports

--- system/modules/base/packages.nix
+++ system/modules/base/packages.nix
@@ -26,4 +26,14 @@
       lfs.enable = true;
     };
   };
+  # required for kde-connect
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 }
